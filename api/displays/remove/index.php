983e0127164eb7dcc72af3dfd53d952b2dc49e7a
Jari Lehtinen <jari@feature.fi>
2020-02-06 13:44:25 +0200

    Fixed variable

--- api/displays/remove/index.php
+++ api/displays/remove/index.php
@@ -5,17 +5,18 @@ header('Cache-Control: no-cache, must-revalidate');
 header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
 header('Content-Type: application/json');
 
-require_once('../../../lib/displays.php');
+require_once('../../../config.php');
+require_once(PATH.'/lib/displays.php');
 
 $displays = new Display\Displays;
 
 // Remove display
-if (isset($_REQUEST['remove']) && $_REQUEST['remove']) {
+if (isset($_REQUEST['id']) && $_REQUEST['id']) {
     if ($displays->removeDisplay($_REQUEST['id'])) {
         echo json_encode(['ok' => 1]);
         exit;
     }
-
-    echo json_encode(['error' => 1]);
-    exit;
 }
+
+echo json_encode(['error' => 1]);
+exit;
